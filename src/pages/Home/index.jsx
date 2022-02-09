54b88177c9743c2e452df207a9c11a4e8c2c3a81
Bieltroll <skolzinlo@hotmail.com>
2022-02-09 01:30:32 -0300

    init project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+function Home() {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-links">
+        <div className="navbar-logo">AS</div>
+        <div className="navbar-item">Features</div>
+        <div className="navbar-item">Solutions</div>
+        <div className="navbar-item">Resources</div>
+        <div className="navbar-item">Pricing</div>
+      </div>
+      <div>
+        <div className="navbar-item">Pricing</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
